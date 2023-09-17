83510fb0dfb55d763f0b055917fc04237a08657f
Davidoar15 <davidoar15@gmail.com>
2023-09-17 18:22:06 -0300

    add/NavBar_content

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import logo from '../../assets/logo21Grados.jpg';
 import style from './NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
 
   return (
     <div className={style.container}>
-      Hola 
+      <div className={style.logoContainer}>
+        <NavLink to='/'>
+          <img src={logo} className={style.logo} alt='logo'/>
+        </NavLink>
+      </div>
+
+      <div className={style.links}>
+        <NavLink to='/productos' className={style.navlink}>
+          <p>Productos</p>
+        </NavLink>
+        <NavLink to='/servicios' className={style.navlink}>
+          <p>Servicios</p>
+        </NavLink>
+        <NavLink to='/nosotros' className={style.navlink}>
+          <p>Nosotros</p>
+        </NavLink>
+      </div> 
     </div>
   )
 }
